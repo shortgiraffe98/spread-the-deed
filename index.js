1901b8038a378944eb239aea4cbbcbfd1dea1a09
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-16 17:25:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+import 'dotenv/config';
 const app = express();
+const port = process.env.DATABASE_PORT || 8800;
 app.use(cors({origin: ["http://127.0.0.1:5500", "https://donation-campaigns-nab.vercel.app"], credentials: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello");
 })
 
-app.listen(8800, () => {
-    console.log("server running");
+app.listen(port, () => {
+    console.log("server running - port " + port);
 })
