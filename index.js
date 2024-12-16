044c4cef99fd3e2840ebe8fe9b490563a7098ee2
shortgiraffe98 <119474685+shortgiraffe98@users.noreply.github.com>
2024-12-16 15:29:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
+app.use(cors({origin: ["http://127.0.0.1:5500", "https://donation-campaigns-nab.vercel.app"], credentials: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello");
 })
 
-app.listen(5432, () => {
+app.listen(8800, () => {
     console.log("server running");
 })
